1e9fc214a24e19add2fc888e805e460cadcb84f9
Binit Gupta <binitguptapro@gmail.com>
2023-07-14 00:44:13 +0530

    added dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Deal Time',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
       ),
