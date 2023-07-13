94557ce021975254765c8f41dabb422c62dcb79b
Binit Gupta <binitguptapro@gmail.com>
2023-07-14 00:56:38 +0530

    colors dependencies

---
+++ lib/commons/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const backgroundColor = Colors.white;
+const darkColor= Colors.black;
+const primaryColor = Colors.indigo;
+const secondaryColor = Colors.blueGrey;
+const tertiaryColor = Colors.greenAccent;
